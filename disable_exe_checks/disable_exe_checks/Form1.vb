c28219412e310fdc263a89d5077e172cc4e0e517
kotoff-studio <76766132+kotoff-studio@users.noreply.github.com>
2021-05-01 09:30:29 +0300

    program which disables notifications while you try to run any .exe
    [01.05.2021] Project added!

---
+++ disable_exe_checks/disable_exe_checks/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer\Security", "DisableSecuritySettingsCheck", 00000001)
+        Console.WriteLine("1: Success!")
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", "1806", 0)
+        Console.WriteLine("2: Success!")
+    End Sub
+End Class
